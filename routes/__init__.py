a7a34e270d1da136bd0a4b1d3a9b15ec9837a2a3
aliffkhuzairi <aliffkhuzairi07@gmail.com>
2026-05-05 19:33:38 +0800

    feat: add global search

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,10 @@
 from .auth_routes import auth_bp
 from .movie_routes import movie_bp
 from .user_routes import user_bp
+from .search_routes import search_bp
 
 def register_routes(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(movie_bp)
     app.register_blueprint(user_bp)
+    app.register_blueprint(search_bp)
---
+++ routes/search_routes.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, redirect, request, session
+from db import db_cursor
+from utils import login_required
+
+search_bp = Blueprint('search', __name__)
+
+@search_bp.route('/search')
+@login_required
+def global_search():
+    query = request.args.get('q', "").strip()
+    search_pattern = f"%{query}%"
+
+    movies = []
+    users = []
+
+    if query:
+        with db_cursor() as cur:
+            cur.execute("""
+                select id, title, director, genre, rel_date
+                from movies
+                where title ilike %s or director ilike %s or genre ilike %s
+                order by title asc
+                limit 10;
+            """, (search_pattern, search_pattern, search_pattern))
+
+            movies = cur.fetchall()
+
+            cur.execute("""
+                select u.id, u.role, ui.name
+                from users u
+                join user_info ui on u.id = ui.id
+                where u.id ilike %s or ui.name ilike %s
+                order by u.id asc
+                limit 10;
+            """,(search_pattern, search_pattern))
+
+            users = cur.fetchall()
+
+
+    return render_template('search.html',
+                           query=query,
+                           movies=movies,
+                           users=users,
+                           user_id=session.get('user_id'))
