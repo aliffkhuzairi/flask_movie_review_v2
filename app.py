2d1e65305140d2d2ec12057989368ac68e2d2c12
aliffkhuzairi <aliffkhuzairi07@gmail.com>
2026-05-19 03:01:18 +0800

    feat: add CSRF protection to all forms

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from utils import time_ago
 from routes import register_routes
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'dev_secret_key')
+
+csrf = CSRFProtect(app)
 
 app.jinja_env.filters["time_ago"] = time_ago
 app.jinja_env.globals['enumerate'] = enumerate
